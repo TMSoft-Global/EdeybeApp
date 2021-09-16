6715e1365be9175fd3ffe7eaebf4bb859e3c303d
Bismark <mccamo51@gmail.com>
2021-09-16 08:02:58 +0000

    update

--- lib/services/address_operations.dart
+++ lib/services/address_operations.dart
@@ -2,13 +2,18 @@ import 'dart:convert';
 
 import 'package:edeybe/services/server_operation.dart';
 
-class AddressOperations extends ServerOperations{
-  addAddressRequest(Map data, Function(Map<String , dynamic> onResponse)){
+class AddressOperations extends ServerOperations {
+  addAddressRequest(Map data, Function(Map<String, dynamic> onResponse)) {
     dynamicRequest(
-        showDialog: true,
-        method: "POST",
-        path: '/account/deliveryaddresses',
-        schema:jsonEncode(data),
-        onResponse: ((val){}));
+      showDialog: true,
+      method: "POST",
+      path: '/account/deliveryaddresses',
+      schema: jsonEncode(data),
+      onResponse: (val) {
+        print(val);
+        if (val != null) {}
+        // onResponse(val);
+      },
+    );
   }
 }
