b4007949367b590102a6d3d56b10cc20710625f3
Bismark <mccamo51@gmail.com>
2021-11-02 17:18:03 +0000

    created a test file

---
+++ lib/screens/product_details_screen/Untitled-1.dart
@@ -0,0 +1,36 @@
+
+  // setQuantity(int productIndex, int newQTY, String proID,{String variantID}) {
+  //   print("11111$newQTY");
+  //   Map<String, dynamic> items = {};
+
+  //   var item = cartItems[productIndex].setQuantity(newQTY);
+  //   cartItems[productIndex] = item;
+  //   items = {
+  //     "items": variantID == null
+  //         ? {
+  //             "${item.productId}": {"quantity": newQTY}
+  //           }
+  //         : {
+  //             "${item.productId}_${item.selectedVariant}": {
+  //               "quantity": item.quantity
+  //             }
+  //           }
+  //   };
+  //   cartItems.forEach((item) {
+  //     print(items);
+  //     // if (item.selectedVariant != null && proID == item.productId) {
+  //     //   items["items"][item.productId] = {"quantity": newQTY};
+  //     // }
+  //     //  else {
+  //     //   items["items"][item.productId] = {"quantity": item.quantity};
+  //     // }
+  //   });
+  //   // operations.updateCart(items, (response) {
+  //   //   // getCartITems();
+  //   //   update();
+  //   // }, handleError);
+  //   // print(cartItems[productIndex].);
+  //   update();
+  // }
+
+ 
