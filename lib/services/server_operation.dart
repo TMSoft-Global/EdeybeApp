6d712d3bad70cb6e2ca8b3a1bc5e0c974fcaf69d
Bismark <mccamo51@gmail.com>
2022-02-28 10:15:21 +0000

    v1.0.1

--- lib/services/server_operation.dart
+++ lib/services/server_operation.dart
@@ -10,13 +10,17 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart' as Response;
 import 'package:http_parser/http_parser.dart';
 
+final String domain = "http://172.18.72.108:5002";
 // final String domain = "http://172.18.72.60:5002";
+// http://172.18.72.108:5002/api
 
-final String domain = "https://api.edeybe.com";
+// final String domain = "https://api.edeybe.com";
 //
 abstract class ServerOperations {
   // final String _domain = "http://172.18.72.60:5002";
-  final String _domain = "https://api.edeybe.com";
+  final String _domain = "http://172.18.72.108:5002";
+
+  // final String _domain = "https://api.edeybe.com";
   // final String _domain = "api.edeybe.com";
   final String accessToken =
       r"3KJALFDKLAkjksoem$jis0*j3ji49509u5tojifk95%#lk33#4kjjksfkjd@$$krjkrkkioaseif$2kjs@kj5l4#";
@@ -109,16 +113,15 @@ abstract class ServerOperations {
           contentType: new MediaType('photo', 'jpg')),
     });
 
-    Response.Response response =
-        await Response.Dio().post("http://172.18.72.108:5002/api/id-card-upload",
+    Response.Response response = await Response.Dio()
+        .post("http://172.18.72.108:5002/api/id-card-upload",
             data: formData,
             options: Response.Options(
                 headers: headers,
                 followRedirects: false,
                 validateStatus: (status) {
                   return status < 500;
-                })
-            );
+                }));
     print(response.statusCode);
     return response;
 
@@ -135,7 +138,8 @@ abstract class ServerOperations {
       method = "POST",
       @required void onResponse(dynamic response),
       void onError(Response.DioError error),
-      bool showDialog = false, var header}) async {
+      bool showDialog = false,
+      var header}) async {
     // show loading dialog
     if (showDialog) {
       isLoading = true;
@@ -162,7 +166,7 @@ abstract class ServerOperations {
       "$_domain/api$path",
       options: Response.Options(
         method: method,
-        headers:header == null ?  headers: header,
+        headers: header == null ? headers : header,
       ),
       data: schema != "" ? schema : null,
     )
